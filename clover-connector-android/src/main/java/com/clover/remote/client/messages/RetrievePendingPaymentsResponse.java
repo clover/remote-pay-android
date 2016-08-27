c79041775d10973a59d67b68589d3e45ebc3d5e2
Blake <blake@clover.com>
2016-08-27 08:59:24 -0600

    removed comment block

--- clover-connector-android/src/main/java/com/clover/remote/client/messages/RetrievePendingPaymentsResponse.java
+++ clover-connector-android/src/main/java/com/clover/remote/client/messages/RetrievePendingPaymentsResponse.java
@@ -22,9 +22,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * Created by blakewilliams on 7/21/16.
- */
 public class RetrievePendingPaymentsResponse extends BaseResponse {
   List<PendingPaymentEntry> pendingPayments;
 
